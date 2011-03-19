f7103cfd9076089abad56b528c65ade3375d83c1
Mutahhir <Mutahhir.ali@gmail.com>
2011-03-19 17:50:35 +0500

    Typewriter effect for end only

--- shared/writeroom/writeroom.js
+++ shared/writeroom/writeroom.js
@@ -8,7 +8,6 @@ define("writeroom/layout", function(require, exports, module) {
         this.editor = editor;
         this.trail = 0;
         this.lead = 0;
-        
     };
     
     LayoutManager.prototype = {
@@ -39,13 +38,20 @@ define("writeroom/layout", function(require, exports, module) {
         	var _this = this;
         	var ed = this.editor;
         	return function(e) { 
-        		var rend = ed.renderer;
-        		var width = dom.getInnerWidth(rend.container);
-        		var charWidth = rend.characterWidth;
-        		var innerWidth = charWidth * blockWidth;
-        		var remaining = width - innerWidth;
+        		var rend = ed.renderer,
+        			width = dom.getInnerWidth(rend.container),
+        			charWidth = rend.characterWidth,
+        			innerWidth = charWidth * blockWidth,
+        			remaining = width - innerWidth;
+        		
         		rend.setPrintMarginColumn(blockWidth);
-        		rend.setPadding(remaining/2);
+        		rend.$padding = remaining/2;
+                rend.content.style["padding-left"] =
+                rend.content.style["padding-right"] =
+                	rend.$padding + "px";
+                rend.$loop.schedule(this.CHANGE_FULL);
+                rend.$updatePrintMargin();
+        		
         		if( _this.trail > 0 ) {
 	            	var height = dom.getInnerHeight(rend.container);
 	            	var lineHeight = rend.lineHeight;
@@ -65,10 +71,16 @@ define("writeroom/layout", function(require, exports, module) {
         
         keepCurrentLineAtCenter: function keepCurrentLineAtCenter() {
         	var _this = this;
-        	event.addListener(this.editor.getSession(), "change", function() {
-        		var ed = _this.editor;
-        		var pos = ed.getCursorPosition();
-        		ed.renderer.scrollToLine(pos.row, true);
+        	var numLines = -1;
+        	event.addListener(this.editor.getSession(), "change", function(e) {
+        		var ed = _this.editor,
+        			lines = ed.session.getLength();
+        		if( numLines == lines ) return;
+        		
+        		var	rend = ed.renderer,
+        			pos = ed.getCursorPosition();
+        		numLines = lines;
+        		_this.paddingAwareScrollToLine(pos.row, true);
         	});
         },
         
@@ -78,16 +90,61 @@ define("writeroom/layout", function(require, exports, module) {
         },
         
         onScroll: function onScroll(e) {
-        	if( this.lead == 0 ) return;
+        },
+        
+        adjustPaddingFor: function adjustPaddingFor(row, dontAdjust) {
         	var rend = this.editor.renderer,
-        		lineHeight = rend.lineHeight,
-            	height = dom.getInnerHeight(rend.container),
-            	atLine = Math.ceil(e.data / lineHeight),
-            	leadLines = (this.lead * height) / lineHeight;
-        	if( atLine > leadLines ) return;
-        	var diff = leadLines - atLine;
-        	var padd = diff * lineHeight;
-        	rend.content.style["padding-top"] = padd+"px";
+	    		lineHeight = rend.lineHeight,
+	        	height = dom.getInnerHeight(rend.container),
+	        	leadLines = Math.round((this.lead * height) / lineHeight),
+	        	prevPadding = parseInt(rend.content.style["padding-top"]);
+	        	mid = (rend.$size.scrollerHeight-prevPadding) / 2,
+	        	midline = mid / lineHeight,
+	        	adjustFlag = typeof(dontAdjust) === "undefined" || dontAdjust !== true; 
+
+        	if( row >= midline ) {
+        		diff = row - midline;
+        		if( diff > leadLines ) {
+        			if( adjustFlag ) { 
+        				rend.content.style["padding-top"] = "0px";
+        				console.log("going zero");
+        			}
+        			
+        			return 0;
+        		} else {
+        			var diff2 = Math.round(leadLines - diff),
+        				pad =  diff2 * lineHeight;
+        			if( pad < lineHeight ) pad = 0;
+        			if( adjustFlag ) { 
+        				rend.content.style["padding-top"] = pad+"px";
+        				console.log("padding: "+pad);
+        			}
+        			return diff;
+        		}
+        	}
+        	
+        	if( adjustFlag ){ 
+        		rend.content.style["padding-top"] = (leadLines*lineHeight)+"px";
+        		console.log("full padding / r:"+row+" m:"+midline);
+        	}
+        	
+        	return leadLines;
+        },
+        
+        paddingAwareScrollToLine: function(line, center) {
+            var offset = 0;
+            var rend = this.editor.renderer,
+            	lineHeight = { lineHeight: rend.lineHeight };
+            	
+            for (var l = 1; l < line; l++) {
+                offset += this.editor.session.getRowHeight(lineHeight, l-1);
+            }
+            
+            if (center) {
+                offset -= (rend.$size.scrollerHeight) / 2 ;
+            }
+            
+            rend.scrollToY(offset);
         }
         
     };
