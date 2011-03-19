48af182eef5a32f0995c219539f8217ffbf733a6
Mutahhir <Mutahhir.ali@gmail.com>
2011-03-19 06:45:11 +0500

    Typewriter session is now properly integrated

--- shared/writeroom/typewriter_session.js
+++ shared/writeroom/typewriter_session.js
@@ -1,15 +1,31 @@
 define("writeroom/typewriter_session", function(require, exports, module) {
-    
-    var oop = require("pilot/oop");
-    var EditSession = require("ace/edit_session").EditSession;
-    
-    var TWSession = function TWSession(text, mode) {
-        // Calling the EditSession constructor on our object, so we look like EditSession
-    	EditSession.prototype.constructor.apply(this, arguments);
-    };
-    
-    
-    oop.inherits(TWSession, EditSession);
-    
-    exports.TypewriterSession = TWSession;
+
+	var oop = require("pilot/oop");
+	var EditSession = require("ace/edit_session").EditSession;
+
+	var TWSession = function TWSession(text,mode) {
+		// Calling the EditSession constructor on our object, so we look like EditSession
+		EditSession.prototype.constructor.apply(this, arguments);
+
+	};
+
+	oop.inherits(TWSession, EditSession);
+
+	(function() {
+		
+		this.getScreenLength = function getScreenLength() {
+			var screenRows = TWSession.super_.getScreenLength.call(this);
+			
+			var ed = this.editor;
+			
+			return screenRows;
+		};
+
+
+	}).call(TWSession.prototype);
+
+
+	
+
+	exports.TypewriterSession = TWSession;
 });
