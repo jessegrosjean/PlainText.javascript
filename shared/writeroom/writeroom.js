706788cb3235a04ae4da83bb469a23e623031a8a
Mutahhir <Mutahhir.ali@gmail.com>
2011-03-20 21:28:58 +0500

    Added *not* working word counter, under development

--- shared/writeroom/writeroom.js
+++ shared/writeroom/writeroom.js
@@ -16,5 +16,36 @@ define("writeroom/base", function(require, exports, module) {
 				nativeDocument.javascriptUpdateChangeCount_(5 /* NSChangeRedone */);
 		});	
 	}
+	
+	exports.getWordCountListener = function(editor, updateFn) {
+		var lastWasSeperator = false;
+		return function(e) { 
+			var delta = e.data;
+			var wordDelta = 0;
+			var wordArray = [];
+			if( delta.action === "insertText" ) {
+				if( delta.text.match(/^(\W|\s)+$/) !== null  ) {
+					lastWasSeperator = true;
+				} else {
+					if( lastWasSeperator ) {
+						wordDelta = 
+					}
+					lastWasSeperator = false;
+				}
+				if( wordDelta > 0 ) wordDelta -= 1;
+				updateFn(wordDelta);
+			} else if( delta.action === "removeText" ) {
+				wordArray = delta.text.replace(/\W+/g, " ").split(" ");
+				for( var i = 0; i < wordArray.length; i++ ) {
+					if( wordArray[i] !== "" ) {
+						wordDelta += 1;
+					}
+				}
+				if( wordDelta > 0 ) wordDelta -= 1;
+				updateFn(-wordDelta)
+			}
+		}
+		
+	}
 
 });
