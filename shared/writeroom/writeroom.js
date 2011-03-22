6957bbcfb54e5433041ffb534299188332fca7c3
Mutahhir <Mutahhir.ali@gmail.com>
2011-03-22 18:19:59 +0500

    Word Count when inserting text should work properly now

--- shared/writeroom/writeroom.js
+++ shared/writeroom/writeroom.js
@@ -18,34 +18,162 @@ define("writeroom/base", function(require, exports, module) {
 	}
 	
 	exports.getWordCountListener = function(editor, updateFn) {
-		var lastWasSeperator = false;
 		return function(e) { 
-			var delta = e.data;
-			var wordDelta = 0;
-			var wordArray = [];
-			if( delta.action === "insertText" ) {
-				if( delta.text.match(/^(\W|\s)+$/) !== null  ) {
-					lastWasSeperator = true;
-				} else {
-					if( lastWasSeperator ) {
-						wordDelta = 
-					}
-					lastWasSeperator = false;
+			var delta = e.data,
+				range = delta.range,
+				beforeStart = exports.movePointLeftInline(editor, range.start),
+				afterEnd = exports.movePointRightInline(editor, range.end);
+				
+			var text = "";
+			if( delta.action === "insertText" || delta.action === "removeText" )
+				text = delta.text;
+			else if ( delta.action === "insertLines" || delta.action === "removeLines" ) 
+				text = delta.lines.join(" ");
+			
+			
+			var textstats = exports.countWords(text);
+			
+			if( delta.action === "insertText" || delta.action === "insertLines" ){
+				var appendingToWord = false, prependingToWord = false, breakingWord = false;
+				
+				if( beforeStart == null && afterEnd == null) {
+					updateFn(textstats.words);
+					return;
 				}
-				if( wordDelta > 0 ) wordDelta -= 1;
-				updateFn(wordDelta);
-			} else if( delta.action === "removeText" ) {
-				wordArray = delta.text.replace(/\W+/g, " ").split(" ");
-				for( var i = 0; i < wordArray.length; i++ ) {
-					if( wordArray[i] !== "" ) {
-						wordDelta += 1;
-					}
+				
+				if( beforeStart !== null ) {
+					var beforeChar = editor.session.getLine(beforeStart.row).charAt(beforeStart.column);
+					appendingToWord = beforeChar.length > 0 && beforeChar.match(/\s/) === null && textstats.startsWithAWord;
 				}
-				if( wordDelta > 0 ) wordDelta -= 1;
-				updateFn(-wordDelta)
+				
+				if(afterEnd !== null){
+					var afterChar = editor.session.getLine(range.end.row).charAt(range.end.column);
+					prependingToWord = afterChar.length > 0 && afterChar.match(/\s/) === null && textstats.endsWithAWord;
+				}
+				
+				breakingWord = beforeStart && afterEnd && beforeChar.match(/\s/) === null && afterChar.match(/\s/) === null;
+				
+				if( !appendingToWord && !prependingToWord ) {
+					updateFn(textstats.words + (breakingWord? 1:0));
+					return;
+				}
+				
+				if( appendingToWord && prependingToWord ) {
+					if( textstats.words === 0 ) updateFn(1);
+					else updateFn(textstats.words-1);
+					return;
+				}
+				
+				if( appendingToWord || prependingToWord ) {
+					if( textstats.words !== 0 ) 
+						updateFn(textstats.words - 1);
+					return;
+				}
 			}
+			
+		}
+	};
+	
+	exports.countWords = function(text) {
+		var sepRe = /\s/g;
+		text = text.replace(sepRe, ' ');
+		var words = text.split(' ');
+		var count = 0;
+		for( var i = 0; i < words.length; i++ ) {
+			if( words[i].length > 0 )	count++;
 		}
 		
-	}
+		return {
+			"words": count, 
+			"startsWithAWord": words[0].length > 0,
+			"endsWithAWord": words[words.length-1].length > 0
+		};
+	};
+	
+	/* Taken from selection.moveCursorLeft
+	 * 
+	 * This only moves the position instead of the cursor
+	 * 
+	 */
+	exports.movePointLeft = function(editor, pos){
+		var newPos;
+		if(pos.column === 0) { 
+			if( pos.row > 0 ){
+				newPos = { row: pos.row-1, column: editor.session.doc.getLine(pos.row-1).length };
+			} else { 
+				newPos = null;	// we're at document start, can't move left
+			}
+		} else {
+			var tabSize = editor.session.getTabSize();
+            if (editor.session.isTabStop(pos) && editor.session.doc.getLine(pos.row).slice(pos.column-tabSize, pos.column).split(" ").length-1 == tabSize)
+                newPos = {row: pos.row, column: pos.column-tabSize};
+            else
+                newPos = {row: pos.row, column: pos.column-1};
+		}
+		
+		return newPos;
+	};
+	
+	exports.movePointLeftInline = function(editor, pos){
+		var newPos;
+		if(pos.column === 0) { 
+			newPos = null;	// we're at document start, can't move left
+		} else {
+			var tabSize = editor.session.getTabSize();
+            if (editor.session.isTabStop(pos) && editor.session.doc.getLine(pos.row).slice(pos.column-tabSize, pos.column).split(" ").length-1 == tabSize)
+                newPos = {row: pos.row, column: pos.column-tabSize};
+            else
+                newPos = {row: pos.row, column: pos.column-1};
+		}
+		return newPos;
+	};
+	
+	exports.canMoveLeft = function(editor, pos) {
+		return pos.column === 0 && pos.row === 0;
+	};
+	
+	exports.canMoveRight = function(editor, pos) {
+		if( pos.row === editor.session.doc.getLength() - 1 ) {
+			return pos.column !== editor.session.doc.getLine(pos.row).length;
+		} 
+		return true;
+	};
+	
+	/*
+	 *	Taken from selection.moveCursorRight 
+	 */
+	exports.movePointRight = function(editor, pos) {
+		var newPos;
+		if (pos.column == editor.session.doc.getLine(pos.row).length) {
+            if (pos.row < editor.session.doc.getLength() - 1) {
+                newPos = {row: pos.row+1, column: 0};
+            } else {
+            	newPos = null;
+            }
+        }
+        else {
+            var tabSize = editor.session.getTabSize();
+            if (editor.session.isTabStop(pos) && editor.session.doc.getLine(pos.row).slice(pos.column, pos.column+tabSize).split(" ").length-1 == tabSize)
+                newPos= {row: pos.row, column: pos.column+tabSize};
+            else
+            	newPos = {row: pos.row, column: pos.column+1};
+        }
+		return newPos;
+	};
+
+	exports.movePointRightInline = function(editor, pos) {
+		var newPos;
+		if (pos.column == editor.session.doc.getLine(pos.row).length) {
+        	newPos = null;
+        }
+        else {
+            var tabSize = editor.session.getTabSize();
+            if (editor.session.isTabStop(pos) && editor.session.doc.getLine(pos.row).slice(pos.column, pos.column+tabSize).split(" ").length-1 == tabSize)
+                newPos= {row: pos.row, column: pos.column+tabSize};
+            else
+            	newPos = {row: pos.row, column: pos.column+1};
+        }
+		return newPos;
+	};
 
 });
