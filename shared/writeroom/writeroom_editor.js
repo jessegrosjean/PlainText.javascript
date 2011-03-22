211a5d6756ddd8b704ca64e5a5d531b093cacee9
Mutahhir <Mutahhir.ali@gmail.com>
2011-03-22 20:53:54 +0500

    Toggle formatting commands now handle selections better

--- shared/writeroom/writeroom_editor.js
+++ shared/writeroom/writeroom_editor.js
@@ -3,6 +3,7 @@ define("writeroom/editor", function(require, exports, module) {
 	var oop = require("pilot/oop");
 	var canon = require("pilot/canon");
 	var Editor = require("ace/editor").Editor;
+	var Range = require("ace/range").Range;
 	require("writeroom/commands");
     
 	   
@@ -18,23 +19,40 @@ define("writeroom/editor", function(require, exports, module) {
 			if (this.$readOnly)
 				return;
 			
+			var curSelectionRange = this.getSelectionRange();
+			
 			if( this.session.getSelection().isEmpty() ) {
-				var pos = this.getCursorPosition();
-				var range = this.session.getWordRange(pos.row, pos.column);
-				this.session.getSelection().setSelectionRange(range, false);
+				var curPos = this.getCursorPosition();
+				var ins = boundary+" "+boundary;
+				this.session.insert(curPos, ins);
+				var range = new Range(curPos.row, curPos.column+boundary.length, 
+						curPos.row, curPos.column+boundary.length+1);
+				this.session.selection.setSelectionRange(range, false);
+				return;
 			}
 			
 			var len = boundary.length; 	 
 			var selectedText = this.getCopyText();
+			var range = this.getSelectionRange();
+			var newRange = range;
 			
 			if( selectedText.substr(0, len) === boundary && 
 					selectedText.substr(-len) === boundary ) {
+				// removing
 				selectedText = selectedText.substr(len, selectedText.length-(len*2));
-				this.session.replace(this.getSelectionRange(), selectedText);
+				newRange = new Range(range.start.row, range.start.column, 
+						range.end.row, range.end.column-boundary.length*2);
+				this.session.replace(range, selectedText);
 			} else {
+				// adding
 				selectedText = boundary + selectedText + boundary;
-				this.session.replace(this.getSelectionRange(), selectedText);
+				this.session.replace(range, selectedText);
+				newRange = new Range(range.start.row, range.start.column,
+						range.end.row, range.end.column+boundary.length*2);
 			}
+
+			this.session.selection.setSelectionRange(newRange, false);
+
 		};
 	 
 		this.toggleBold = function(options) {
