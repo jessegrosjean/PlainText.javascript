3f8d58092659f1849d9da21dd22decfa5e987438
Mutahhir <mutahhir.ali@gmail.com>
2011-03-28 23:16:42 +0500

    line height jumpiness solved 2

--- shared/writeroom/writeroom_layout.js
+++ shared/writeroom/writeroom_layout.js
@@ -122,12 +122,12 @@ define("writeroom/layout", function(require, exports, module) {
 			
 			setLineHeight: function setLineHeight(fsz) {
 				document.getElementById("editor").style["line-height"] = fsz;
-				editor.renderer.$textLayer.checkForSizeChanges();
+				editor.renderer.updateFontSize();
 			},
 			
 			setFontSize: function setFontSize(fsz) {
 				document.getElementById("editor").style.fontSize = fsz;
-				editor.renderer.$textLayer.checkForSizeChanges();
+				editor.renderer.updateFontSize();
 			}
 
 	};
